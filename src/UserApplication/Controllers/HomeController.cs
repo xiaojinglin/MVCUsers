bdedd37617ce942c3794cdd52391d8ec86a680aa
xiaojinglin <candylinxiaojing@gmail.com>
2017-02-13 09:39:40 -0500

    First commit!

---
+++ src/UserApplication/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UserApplication.Models;
+
+namespace UserApplication.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult REG()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult REG(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thank", user);
+            }
+            else
+            {
+                return View();
+            }
+
+        }
+
+    }
+}
---
+++ src/UserApplication/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace UserApplication.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter a username")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter a password")]
+        public string PassWord { get; set; }
+
+        [Required(ErrorMessage = "Please enter a phone number")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Please enter an email")]
+        [RegularExpression(".+\\@.+\\..+", ErrorMessage = "Please enter a regular Email address")]
+        public string Email { get; set; }
+    }
+}
